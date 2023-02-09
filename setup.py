a15f553ab05748de7a79d15a458acb7ca86da436
Rogerio Rodrigues <rogeriodemoraes@gmaol.com>
2023-02-09 15:37:53 -0300

    chenged gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="trade",
+    version="0.1.0",
+    description="A quantitative trade training",
+    author="Rogerio Rodrigues", 
+    packages=find_packages(),
+)
--- trade/core.py
+++ trade/core.py
@@ -1,5 +1,10 @@
-from mt5 import conecta_mt5
+from trade.mt5 import conecta_mt5
 
 
 def main():
     conecta_mt5("BTG")
+
+
+main()
+
+print("Rodou a main() function")
